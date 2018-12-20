b6cee941df6c36604eb6681646e3539428c3185c
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-20 22:54:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,8 @@
 
 
 
-def introduction(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
+
+
+#def introduction(name, language)
+ # puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#end
